7e168bd79f1d131a01dd35648792fa0df8a6e097
Bob-the-Bobothy <126205683+Bob-the-Bobothy@users.noreply.github.com>
2023-03-28 08:43:04 -0700

    Added rock.php, fixed map.php navbar

--- php/map.php
+++ php/map.php
@@ -2,7 +2,7 @@
 <html>
   <head>
     <meta charset="UTF-8">
-    <title>Turlock Rock and Gem Show Map</title>
+    <title>Turlock Rock and Gem Show- Map</title>
     <link rel="stylesheet" href="/css/style.css">
   </head>
   <body>
---
+++ php/rock.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Turlock Rock and Gem Show Rock Sale</title>
+    <link rel="stylesheet" href="/css/style.css">
+  </head>
+  <body>
+    <header>
+      <h1 href="index.php">Turlock Rock and Gem Show</h1>
+      <nav>
+        <ul>
+          <li><a href="map.php">Home</a></li>
+        </ul>
+      </nav>
+    </header>
+    <footer>
+      <p>&copy; 2023 Turlock Rock and Gem Show</p>
+    </footer>
+  </body>
+</html>
